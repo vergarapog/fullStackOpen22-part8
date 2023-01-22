414e5ce66e60a7d65979e5e1a424b0939b3d8951
vergarapog <201801166@iacademy.edu.ph>
2023-01-22 14:32:44 +0800

    finished exercise 8.4

--- exercise_8_1to8_7/index.js
+++ exercise_8_1to8_7/index.js
@@ -112,7 +112,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -121,7 +121,19 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+
+      let booksByAuthor = books.filter((book) => {
+        if (book.author === args.author) {
+          return book
+        }
+      })
+
+      return booksByAuthor
+    },
     allAuthors: () => authors,
   },
   Author: {
